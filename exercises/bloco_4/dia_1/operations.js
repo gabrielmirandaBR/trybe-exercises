bcd193efc13fca1003e1f1176ecdf0b3f2c3c32a
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-02-24 15:42:22 -0300

    primeiros passos com JS - variaveis e operadores

---
+++ exercises/bloco_4/dia_1/operations.js
@@ -0,0 +1,16 @@
+let base = 5;
+let height = 8;
+console.log(base);
+console.log(height);
+
+let area = (base * height);
+console.log(area);
+
+let thirdSide = Math.floor(Math.sqrt((base**2)+(height**2)));
+console.log(thirdSide);
+
+let perimeter = (base + height + thirdSide);
+console.log(perimeter);
+
+
+
---
+++ exercises/bloco_4/dia_1/scripts.js
@@ -0,0 +1,11 @@
+const name = "Gabriel";
+
+const birthCity = "Barbacena";
+birthCity = "Juiz de Fora" // Não é Possível alterar o valor de uma constante e, por isso, acontence o erro
+
+let birthYear = 1992;
+birthYear = 2020;
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
