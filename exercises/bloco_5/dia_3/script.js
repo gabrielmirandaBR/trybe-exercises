67a60a03602d73a99e0801b8c2ff88f9b4ad3c21
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-05 18:45:26 -0300

    atualiza  exercicio 1

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -14,20 +14,25 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 let accessList = document.querySelector('#days');
 
 for (index = 0; index < dezDaysList.length; index += 1) {
   let item = dezDaysList[index];
-
   let listItem = document.createElement('li');
   listItem.className = 'day';
   listItem.innerText = item;
+
+  if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31) {
+    listItem.className = 'day holiday';
+  } else if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25) {
+    listItem.className = 'day friday';
+  }
   
   accessList.appendChild(listItem);
   console.log(listItem);
-
-
- 
 }
+
+
