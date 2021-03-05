d8f3f7612d85821c6e3df3b82ccf40fd577df365
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-05 18:42:06 -0300

    add exercicio 1

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -14,3 +14,20 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let accessList = document.querySelector('#days');
+
+for (index = 0; index < dezDaysList.length; index += 1) {
+  let item = dezDaysList[index];
+
+  let listItem = document.createElement('li');
+  listItem.className = 'day';
+  listItem.innerText = item;
+  
+  accessList.appendChild(listItem);
+  console.log(listItem);
+
+
+ 
+}
