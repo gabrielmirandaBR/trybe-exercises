7cf2f220391c5403f09e0075e3125c8fd8585858
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-23 18:08:40 -0300

    add exercicios parte 1

---
+++ exercises/bloco_7/dia_1/script.js
@@ -0,0 +1,37 @@
+// Parte I
+
+// 1
+/* function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+ */
+
+const testingScope = escopo => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+//2
+const sortFunction = (a, b) => (a - b)
+
+const sortArray = (array) => array.sort(sortFunction);
+
+console.log(`os números ${sortArray([13, 3, 4, 10, 7, 2])} se encontram ordenados de forma crescente`);
