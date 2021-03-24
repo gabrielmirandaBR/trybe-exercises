35e2392018671d8fa9bd325362b217cbafc3bb51
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-24 18:31:15 -0300

    add exercicio 3 - Parte 2

--- exercises/bloco_7/dia_2/script2.js
+++ exercises/bloco_7/dia_2/script2.js
@@ -20,12 +20,23 @@ const lesson3 = {
 
 /* 1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. */
 
-function createTurn (obj, key, value) {
+const createTurn = (obj, key, value) => {
   let newKey = key;
   const newValue = value;
   obj[newKey] = newValue;
   
   return obj;
-}
+};
 
 console.log(createTurn(lesson2, 'turno', 'manhã'));
+
+/* 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro. */
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+const showKeys = (obj) => Object.keys(obj);
+
+console.log(showKeys(lesson3));
