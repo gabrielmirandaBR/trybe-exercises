d64bc057d85d25c113c31326f3949e0cef6a75c1
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-29 15:59:14 -0300

    add ex. 1

---
+++ exercises/bloco_8/dia_1/script.js
@@ -0,0 +1,19 @@
+const functionFirstClass = () => {
+  console.log('Acordando!');
+}
+
+const functionFirstClass2 = () => {
+  console.log('Bora tomar café!');
+}
+
+const functionFirstClass3 = () => {
+  console.log('Partiu dormir!');
+}
+
+const doingThings = (function1) => {
+  function1;
+}
+
+doingThings(functionFirstClass());
+doingThings(functionFirstClass2());
+doingThings(functionFirstClass3());
