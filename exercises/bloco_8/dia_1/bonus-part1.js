ec35e2d31bcba71aac2c5da15c06d51cf7f9d3eb
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-30 10:25:00 -0300

    add ex 2 - bonus parte 2

--- exercises/bloco_8/dia_1/bonus-part1.js
+++ exercises/bloco_8/dia_1/bonus-part1.js
@@ -66,15 +66,28 @@ const mageDamage = () => {
 
 /* Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior . */
 
+/* Crie a segunda HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem mage . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem mage e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor das chaves damage e mana do mage. */
+
 
 const gameActions = {
 
   warriorTurn: (warriorDamage) => {
     const damage = warriorDamage();
+
     dragon.healthPoints -= damage;
     warrior.damage = damage;
-    return dragon;
   },
+
+  mageTurn: (mageDamage) => {
+    const objMage = mageDamage();
+    const damage = objMage.damage;
+    const mana = objMage.spentMana;
+
+    dragon.healthPoints -= damage;
+    mage.damage = damage;
+    mage.mana -= mana;
+
+  }
 };
 
-console.log(gameActions.warriorTurn(warriorDamage));
+
