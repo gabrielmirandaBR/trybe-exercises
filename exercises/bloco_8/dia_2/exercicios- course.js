947762cb9bae6db6ba2ccd423347bcc201895c68
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-30 15:54:28 -0300

    add exemplo course

---
+++ exercises/bloco_8/dia_2/exercicios- course.js
@@ -0,0 +1,52 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// Use o método forEach chamando a callback showEmailList para apresentar os emails.
+emailListInData.forEach((email) => {
+  showEmailList(email);
+})
+
+// ex
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+// ex
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
+
+
+// ex find
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const verifyEven = (number) => number % 2 === 0;
+
+const isEven = numbers.find(verifyEven);
+
+console.log(isEven); // 30
+
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Outra forma de ser realizada sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+
+console.log(isEven2); // 30
