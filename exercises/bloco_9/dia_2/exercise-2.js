9a013abe32d1a7e3d1124af576f8ecfbd2fd5176
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-13 19:15:20 -0300

    add exercicios 1 e 2

---
+++ exercises/bloco_9/dia_2/exercise-2.js
@@ -0,0 +1,19 @@
+const promise = new Promise((resolve, reject) => {
+  const numbers = [];
+
+  for (let index = 0; numbers.length < 10; index += 1) {
+  numbers.push(Math.floor(Math.random() * 50));
+  }
+
+  const numbers2 = numbers.map((number) => number ** 2);
+
+  const sumNumbers = numbers2.reduce((acumulator, currentValue) => acumulator + currentValue);
+
+  if (sumNumbers > 8000) {
+    return reject(sumNumbers);
+  }
+
+  resolve(sumNumbers);
+})
+.then((number) => console.log(`${number} - Promise resolvida`))
+.catch((number) => console.log(`${number} -Promise Inválida`));
--- exercises/bloco_9/dia_2/exercises.js
+++ exercises/bloco_9/dia_2/exercises.js
@@ -5,14 +5,18 @@ R.: É um código que é executado simultaneamente com outros códigos, ou seja,
 Já um código síncrono depende do retorno de outra função para depois ser executado, roda um código de cada vez, respeitando a ordem de execução passada.
 
 2 - Quando você tem que enfileirar várias callbacks , que problema surge?
+
 R.: Surge o fenômeno de callback hell, ou seja, o código fica muito profundo, difícil de manter e de ler.
 
 3 - Por que as Promises são uma forma de se resolver esse problema?
+
 R.: As promises são por sua natureza assíncronas e apresentam funções especiais que facilitam o seu uso como then e catch.
 
 4 - Quando você vai se comunicar com uma API , tal comunicação deve ser síncrona ou assíncrona? Lembre-se de que o serviço ao qual você está se conectando pode demorar muito ou pouco para dar retorno, pode estar fora do ar etc.
+
 R.: A comunicação deve ser assíncrona, ou seja, enquanto espera a resposta do servidor, outras funções podem continuar suas execuções sem ser afetadas.
 
 5 - Dada a resposta anterior, o que é fetch e para que ele serve? 
+
 R.: o fetch é uma função responsável por enviar requisições a APIs, além de ter a capacidade de tratar os dados recebidos e devolvê-los.
 */
