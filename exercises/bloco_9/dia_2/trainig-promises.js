3611d2a2af3581cedf63fd8d3b82125ad412c93f
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-14 08:35:20 -0300

    commit final

--- exercises/bloco_9/dia_2/trainig-promises.js
+++ exercises/bloco_9/dia_2/trainig-promises.js
@@ -4,8 +4,10 @@ const promise = new Promise((resolve, reject) => {
   if (number <= 5) {
     return reject(number);
   }
+  
   resolve(number);
-})
+});
+promise
 .then((number) => `Que sucesso =) nosso número foi ${number}`)
 .then((msg) => console.log(msg))
 .catch((number) => console.log(`Que fracasso =( Nosso número foi ${number}`));
