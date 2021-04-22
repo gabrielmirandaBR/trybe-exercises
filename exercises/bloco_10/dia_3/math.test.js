9a6babc88da755783e6e6a67e71ada154f8400fa
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-22 18:12:02 -0300

    arruma exercicios 1 e 2

--- exercises/bloco_10/dia_3/math.test.js
+++ exercises/bloco_10/dia_3/math.test.js
@@ -1,20 +1,31 @@
 const { somar, subtrair, multiplicar, dividir } = require('./math');
 
-jest.mock('./math');
+jest.mock('./math', () => ({
+  subtrair: jest.fn(),
+  multiplicar: jest.fn().mockReturnValue(10),
+}));
 
+// eslint-disable-next-line max-lines-per-function
 describe('Testes dos exercícios para Fixação', () => {
   test('checa a chamada da função subtrair', () => {
-    subtrair.mockImplementation((a, b) => a - b);
-    subtrair(2, 1);
-
+    subtrair();
     expect(subtrair).toHaveBeenCalled();
   });
 
   test('checa a chamada e o retorno da função multiplicar', () => {
-    multiplicar.mockImplementation((a, b) => a * b);
-    multiplicar(5, 2);
-
+    multiplicar();
     expect(multiplicar).toHaveBeenCalled();
-    expect(multiplicar(5, 2)).toBe(10);
+    expect(multiplicar()).toBe(10);
+  });
+
+/*   test('checa a chamada, o retorno e os parâmetros passados para a função somar', async () => {
+    somar.mockResolvedValue(8);
+    somar();
+    expect(somar).toHaveBeenCalled();
+    expect(somar()).resolves.toBe(8);
+  }); */
+
+  test('checa função dividir', () => {
+    
   });
 });
