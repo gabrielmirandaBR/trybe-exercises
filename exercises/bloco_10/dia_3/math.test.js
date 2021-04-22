43bef413cd4d8acf3efbf1dfedc68158f3369e74
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-22 18:41:15 -0300

    add exercicio 3

--- exercises/bloco_10/dia_3/math.test.js
+++ exercises/bloco_10/dia_3/math.test.js
@@ -3,6 +3,8 @@ const { somar, subtrair, multiplicar, dividir } = require('./math');
 jest.mock('./math', () => ({
   subtrair: jest.fn(),
   multiplicar: jest.fn().mockReturnValue(10),
+  somar: jest.fn(),
+  dividir: jest.fn().mockReturnValue(15),
 }));
 
 // eslint-disable-next-line max-lines-per-function
@@ -18,14 +20,31 @@ describe('Testes dos exercícios para Fixação', () => {
     expect(multiplicar()).toBe(10);
   });
 
-/*   test('checa a chamada, o retorno e os parâmetros passados para a função somar', async () => {
-    somar.mockResolvedValue(8);
-    somar();
+  test('checa a chamada, o retorno e os parâmetros passados para a função somar', async () => {
+    somar(2, 3);
     expect(somar).toHaveBeenCalled();
-    expect(somar()).resolves.toBe(8);
-  }); */
+    expect(somar).toHaveBeenCalledWith(2, 3);
+    // const sum = await somar(1, 5);
+    // expect(sum).toEqual(6);
+  });
 
-  test('checa função dividir', () => {
-    
+  test('checa a função dividir', () => {
+    dividir();
+    expect(dividir).toHaveBeenCalled();
+    expect(dividir()).toBe(15);
+
+    dividir.mockReset();
+    dividir(7, 5);
+    expect(dividir).toHaveBeenCalledWith(7, 5);
+
+    dividir.mockReset();
+    dividir.mockReturnValue(2);
+    expect(dividir()).toBe(2);
+
+    dividir.mockReset();
+    dividir.mockReturnValue(5);
+    expect(dividir()).toBe(5);
+
+    expect(dividir).toHaveBeenCalledTimes(1);
   });
 });
