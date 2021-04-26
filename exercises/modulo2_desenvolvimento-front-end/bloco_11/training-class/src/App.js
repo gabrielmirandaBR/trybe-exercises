ab0b8325ea7860c0591fdff0a64b247d778d5e00
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-26 19:09:13 -0300

    add exercicio course

---
+++ exercises/modulo2_desenvolvimento-front-end/bloco_11/training-class/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Training  from "./Component";
+
+function App() {
+  return <Training />;
+}
+
+export default App;
---
+++ exercises/modulo2_desenvolvimento-front-end/bloco_11/training-class/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Training extends Component {
+  render() {
+    return <div>
+      <h1>Gabriel Miranda</h1>
+      <p>Sou estudante da Trybe.</p>
+    </div>
+  }
+}
+
+export default Training;
