ec0e8051bdb4ba5a8fc4b8d337a56e962d02713f
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-05-21 11:19:57 -0300

    passa 1 teste - criar button

--- exercises/modulo2_desenvolvimento-front-end/bloco_15/dia_1/exercise-todo-list/src/InputTodo.js
+++ exercises/modulo2_desenvolvimento-front-end/bloco_15/dia_1/exercise-todo-list/src/InputTodo.js
@@ -33,6 +33,7 @@ class InputTodo extends Component {
           onChange={(e) => this.changeTextTodo(e.target.value)}
         />
         <input id="btnAdd" type="button" value="Adicionar" onClick={() => this.addItem(textTodo,addTodo)} />
+        <input data-testid="id-remove" type="button" value="Remover" disabled />
       </div>
     );
   }
